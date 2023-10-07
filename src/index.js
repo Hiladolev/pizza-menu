5d2ebad2bf964f6405e319d5f3fcb92af69ed418
Hila <hiladolev1@gmail.com>
2023-10-07 13:18:52 +0300

    pizza's name h2 element->h3 (was too big) and close to the photo

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="pizza spinaci"></img>
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
