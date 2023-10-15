fa4fe217679346416b41ff93d25e63daccdd23ff
Hila <hiladolev1@gmail.com>
2023-10-15 16:56:51 +0300

    if statement for not rendering the pizza if it's sold out

--- src/index.js
+++ src/index.js
@@ -96,6 +96,7 @@ function Footer() {
   );
 }
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.name} />
