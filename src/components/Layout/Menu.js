e54c903d3fc4db4460d2417cbe0dc3862c4988d3
Hila <hiladolev1@gmail.com>
2023-10-15 18:55:05 +0300

    an about paragraph before the pizzas list

--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -6,11 +6,18 @@ export default function Menu({ pizzaData }) {
     <main className="menu">
       <h2>our menu</h2>
       {numOfPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Indulge in the flavors of Italy with our authentic Italian cuisine.
+            Our menu features a tantalizing selection of six creatively crafted
+            dishes, each meticulously prepared in our traditional stone oven.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu.Please come back later😋</p>
       )}
